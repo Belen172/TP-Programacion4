f8f6fbceffda58e9d6a667f4d2ae3c765d73a856
Bele Zbrun <zbrunbelu@gmail.com>
2025-10-23 13:38:57 -0300

    actualizacion carpeta frontend

--- FRONTEND/src/features/Productos/types/ProductoTypes.ts
+++ FRONTEND/src/features/Productos/types/ProductoTypes.ts
@@ -1,9 +1,29 @@
 export interface ProductoCrearDto {
-    nombre: string
+    titulo: string
     precio: number
+    descripcion: string
+    slug: string
+    stock: number
+}
+
+export interface ProductoCrearForm {
+    titulo: string
+    precio: string
+    descripcion: string
+    slug: string
+    stock: string
 }
 
 export interface ProductoActualizarDto {
     nombre: string
     precio: number
+}
+
+export interface Producto {
+    id: number
+    titulo: string
+    precio: number
+    descripcion: string
+    slug: string
+    stock: number
 }
--- FRONTEND/src/features/Productos/views/ProductosPage.tsx
+++ FRONTEND/src/features/Productos/views/ProductosPage.tsx
@@ -1,19 +1,65 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { ProductosService } from './../services/ProductosService'
+import type { Producto } from "./../types/ProductoTypes"
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { useNavigate } from 'react-router';
+import { AgGridReact } from 'ag-grid-react';
+
 
 export default function ProductosPage() {
-    const [productos, setProductos] = useState()
+    const [productos, setProductos] = useState<Producto[]>()
+    const [colDefs, setColDefs] = useState([
+        { field: "id", headerName: 'Identificador' },
+        { field: "titulo" },
+        { field: "precio" },
+        { field: "descripcion" },
+        { field: "slug" },
+        { field: "stock" }
+    ]);
+    const rowSelection = useMemo(() => { 
+        return {
+            mode: 'singleRow'
+        };
+    }, []);
     useEffect(() => {
         async function fn () {
             const resultado = await ProductosService.obtenerProductos()
             setProductos(resultado)
-        } 
+        }
         fn()
     }, [])
+    const navigate = useNavigate();
+    function handleButtonNavClickCrear () {
+        navigate("crear")
+    }
+    function handleButtonNavClickEditar () {
+        navigate("editar")
+    }
+    function handleButtonNavClickEliminar () {
+        navigate("eliminar")
+    }
+
     return (
         <>
+            <div>
+                <Box sx={{ width: '100%' }}>
+                    <ButtonGroup variant="outlined">
+                        <Button onClick={handleButtonNavClickCrear}>Crear</Button>
+                        <Button onClick={handleButtonNavClickEditar}>Editar</Button>
+                        <Button onClick={handleButtonNavClickEliminar}>Eliminar</Button>
+                    </ButtonGroup>
+                </Box>
+            </div>
             <h2>Productos</h2>
-            <div>{ productos && productos.map((el) => <li>{el}</li>)  }</div>
+            <div>
+                <div style={{ height: 500 }}>
+                <AgGridReact
+                    rowData={productos}
+                    columnDefs={colDefs} rowSelection={rowSelection}/>
+            </div>
+            </div>
         </>
     )
 }
